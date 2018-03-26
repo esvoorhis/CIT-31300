fdad01f4db5c1548731aeaeffc0bf46884312584
esvoorhis <31226061+esvoorhis@users.noreply.github.com>
2018-03-25 23:50:43 -0400

    Add files via upload

--- ex4/application/controllers/logincontroller.php
+++ ex4/application/controllers/logincontroller.php
@@ -1,18 +1,18 @@
 <?php
-
 class LoginController extends Controller{
-	
-	
-   
-   public function add($num1 = 0,$num2 = 0,$num3 = 0){
-	   
+    protected $userObject;
 
-		$sum = $num1+$num2+$num3;
+    public function do_login() {
+        // handle login
+        $this->userObject = new Users();
+        if($this->userObject->checkUser($_POST['email'],$_POST['password'])) {
+            $userInfo = $this->userObject->getUserFromEmail($_POST['email']);
+            $_SESSION['uID'] = $userInfo['uID'];
+            header('Location: '.BASE_URL);
+        }
+        else {
+            $this->set('error','Wrong password / email combination');
+        }
+    }
 
-	   
-	   $this->set('numbers',$sum);
-	   
-   }
-	
-	
 }
--- ex4/application/controllers/registercontroller.php
+++ ex4/application/controllers/registercontroller.php
@@ -1,17 +1,21 @@
 <?php
 class RegisterController extends Controller{
-    protected $userObject;
 
+    protected $userObject;
     public function defaultTask(){
         $userObject = new Users();
         $this->set('task','add');
     }
- //   public function add(){
- //       $this->userObnect = new Users();
- //       $data = array('first_name'=> $_POST['first_name'],'last_name'=> $_POST['last_name'],
- //           'email'=> $_POST['email'],'password'=> $_POST['password']);
- //       $this->userObject->addUser($data);
- //       $this->set('message','You are now registered.');
- //   }
-    public function do_login(){}
+
+    public function add(){
+        $this->userObject = new Users();
+        $password = $_POST['password'];
+        $passhash = password_hash($password,PASSWORD_DEFAULT);
+        $data = array('first_name'=>$_POST['first_name'],'last_name'=>$_POST['last_name'],'email'=>$_POST['email'],'password'=>$passhash);
+
+        $this->userObject->addUser($data);
+        $this->set('message', 'Thanks for registering!');
+    }
+
 }
+?>
