d3e4924de46213788f6b9017186071f6cffe23db
esvoorhis <evoorhis@iu.edu>
2018-04-05 22:32:03 -0400

    register - works

--- ex6a/application/controllers/registercontroller.php
+++ ex6a/application/controllers/registercontroller.php
@@ -2,14 +2,14 @@
 class RegisterController extends Controller{
 
     protected $userObject;
-    public function defaultTask(){
+    public function index(){
         $userObject = new Users();
         $this->set('task','add');
     }
 
     public function add(){
         $this->userObject = new Users();
-        $password = pasword_hash($_POST['password']);
+        $password = $_POST['password'];
         $passhash = password_hash($password,PASSWORD_DEFAULT);
         $data = array('first_name'=>$_POST['first_name'],'last_name'=>$_POST['last_name'],'email'=>$_POST['email'],'password'=>$passhash);
 
