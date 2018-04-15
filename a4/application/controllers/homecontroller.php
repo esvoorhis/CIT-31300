09ec5f20b66d1c2522842d3dd3c1f2c740fd3a8d
esvoorhis <evoorhis@iu.edu>
2018-04-15 19:53:45 -0400

    rssfeed

--- a4/application/controllers/homecontroller.php
+++ a4/application/controllers/homecontroller.php
@@ -1,18 +1,16 @@
 <?php
 class HomeController extends Controller{
+public function index(){
+$feed = 'http://fox59.com/feed';
 
-    public function index(){
-        $feed = 'http://fox59.com/feed';
-        $rss = new RssDisplay($feed);
+$rss = new RssDisplay($feed);
+$feed_data = $rss->getFeedItems();
 
-        $feed_data = $rss->getFeedItems();
+$channel_title = $feed_data->channel->title;
+$title_title = $feed_data->item->title;
 
-        $channel_title = $feed_data->channel->title;
-        $this->set('rss_title', $channel_title);
-
-
-    }
-
-
+$this->set('rss_title', $channel_title);
+$this->set('article', $title_title);
+}
 }
 ?>
